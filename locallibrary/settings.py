098a0c8c373c4e067a2dcdc0da72d85af09543fa
Ivan Matveev <ivan.matveev@hs-anhalt.de>
2021-02-16 11:18:21 +0100

    Add project structure and db models

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig'
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
